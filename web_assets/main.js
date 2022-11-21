b6b9dd24258c009ca5580177b6ba26f317fcc246
weigq <weigq1234@gmail.com>
2022-11-21 14:15:45 +0800

    update 2211

--- web_assets/main.js
+++ web_assets/main.js
@@ -65,3 +65,17 @@ window.onload = () => {
     for (bibBtn of bibBtns)
         bibBtn.onclick = bibClickFunc;
 }
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
